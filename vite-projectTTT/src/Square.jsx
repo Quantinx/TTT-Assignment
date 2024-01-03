ac108049355b1571ca8ea7c1336d731404d8905d
Quantinx <josephtassell@gmail.com>
2024-01-03 15:29:55 +0100

    Initial upload

---
+++ vite-projectTTT/src/Square.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Square({ value, onSquareClick }) {
+  return (
+    <>
+      <button onClick={onSquareClick} className="square">
+        {value}
+      </button>
+    </>
+  );
+}
